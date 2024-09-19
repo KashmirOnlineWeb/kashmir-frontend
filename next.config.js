60cdce13fe73f9d2a39343551f7107fcc759bc46
Kartic Bedi <me@karticbedi.com>
2024-09-19 21:11:37 +0530

    config file changes

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          source: "/server/:path*",
-          destination: process.env.NEXT_PUBLIC_SERVER_API_URL,
-        },
-      ];
-    }
-  
-  };
-  
-  module.exports = nextConfig;
-  
+  async rewrites() {
+    return [
+      {
+        source: "/server/:path*",
+        destination: process.env.NEXT_PUBLIC_SERVER_API_URL,
+      },
+    ];
+  }
+};
+
+export default nextConfig;
