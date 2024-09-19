c086c2c31b12de554269f96ea182dbb356203af5
Kartic Bedi <me@karticbedi.com>
2024-09-19 21:27:11 +0530

    build

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"NlR0EHUcdqM27O6kK4swrEFSPmhr2oykBsc50JdzI0I=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"9/B/CxyV5uU5KeQgF/G/Af/JfeCm2DlcdJy+0J6msEA=\"}"
--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: false, // Added this line
-  async rewrites() {
-    return [
-      {
-        source: "/server/:path*",
-        destination: process.env.NEXT_PUBLIC_SERVER_API_URL,
-      },
-    ];
-  }
-};
-
-export default nextConfig;
+    async rewrites() {
+      return [
+        {
+          source: "/server/:path*",
+          destination: process.env.NEXT_PUBLIC_SERVER_API_URL,
+        },
+      ];
+    }
+  
+  };
+  
+  module.exports = nextConfig;
+  
